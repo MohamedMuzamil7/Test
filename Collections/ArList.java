516491ee3c94e60cdf5e0766ee465c469b90942a
MohamedMuzamil7 <115407926+MohamedMuzamil7@users.noreply.github.com>
2022-10-09 18:59:49 +0530

    done added
    DONEEE added

--- Collections/ArList.java
+++ Collections/ArList.java
@@ -22,7 +22,7 @@ public class ArList {
 		System.out.println(a);
 		System.out.println(a.remove(3));// remove the specified value by using index
 		System.out.println(a.removeAll(a));// Boolean concept.If it removes all then true or false
-		system.out.println("DONE");
+		system.out.println("DONEEE");
 		
 		a.add(100);
 		a.add("Karthik");
